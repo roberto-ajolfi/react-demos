ee450e39b06e6dbca73ec6162d8280b7bbdb5b1a
Roberto Ajolfi <roberto.ajolfi@icubed.it>
2019-10-08 14:51:25 +0200

    Working on Demo 16 (Redux Form).

---
+++ 16.redux-form/src/App.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import SimpleForm from './SimpleForm';
+import FullForm from './FullForm';
+
+
+import { Provider } from 'react-redux';
+import { createStore, combineReducers } from 'redux';
+import { reducer as reduxFormReducer } from 'redux-form';
+
+const reducer = combineReducers({
+  form: reduxFormReducer
+});
+
+const store = createStore(reducer);
+
+const showResults = (values: any) =>
+  new Promise(resolve => {
+    setTimeout(() => {
+      // delayed for Test purposes
+      const attachmentCount = values.attachment !== undefined && values.attachment.length;
+      window.alert(`You submitted:\n\n${JSON.stringify(values, null, 2)}\n\n(Attachments: ${attachmentCount})`);
+      resolve();
+    }, 500);
+  });
+
+const App: React.FC = () => {
+  return (
+    <Provider store={store}>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          {/* <SimpleForm onSubmit={showResults} /> */} {/* message="Hello Rob!" /> */}
+          <FullForm onSubmit={showResults} />
+        </header>
+      </div>
+    </Provider>
+  );
+};
+
+export default App;
